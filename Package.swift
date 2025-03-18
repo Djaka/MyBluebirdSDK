a0c49b2b5d072159824a22f9185d1e56c4338590
Djaka Permana <djaka.permana@bluebirdgroup.com>
2025-03-18 22:13:23 +0700

    update package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "MyBluebirdSDK",
-            targets: ["MyBluebirdSDK"]
+            targets: ["MyBluebirdSDKTarget"]
         ),
     ],
     dependencies: [],
@@ -16,6 +16,13 @@ let package = Package(
             name: "MyBluebirdSDK",
             url: "https://github.com/Djaka/MyBluebirdSDK/releases/download/v1.0.0/MyBluebirdSDK.xcframework.zip",
             checksum: "450109cf554fbb8d34a154df01dd1145c493ab62fb1762fd766684e91970e4f5"
-        )
+        ),
+        .target(
+            name: "MyBluebirdSDK",
+            path: "compose-resources",
+            resources: [
+                .copy("composeResources")
+            ]
+        ),
     ]
 )
