605c2bf29c770834364f170ff924e0cb23eee550
Djaka Permana <djaka.permana@bluebirdgroup.com>
2025-03-18 11:46:35 +0700

    first commit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyBluebirdSDK",
-            url: "https://drive.google.com/file/d/1nMFdEDgVBnlTKUJkQnOkepSM9jEAh0F_/view?usp=share_link",
+            url: "https://drive.google.com/uc?export=download&id=1nMFdEDgVBnlTKUJkQnOkepSM9jEAh0F_",
             checksum: "450109cf554fbb8d34a154df01dd1145c493ab62fb1762fd766684e91970e4f5"
         ),
         .target(
